8a6f206aca21e9189534d5e28d358139c164a9b0
ddkronac-developer <dd.kronac@seznam.cz>
2026-03-06 11:40:00 +0100

    Update smarty.py

--- scrapers/smarty.py
+++ scrapers/smarty.py
@@ -6,7 +6,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-BASE_SEARCH_URL = "https://www.smarty.cz/Vyhledavani?SearchText={query}"
+BASE_SEARCH_URL = https://www.smarty.cz/Vyhledavani?SearchText={query}
 BASE_URL = "https://www.smarty.cz"
 SCRAPER_API_KEY = os.environ.get("SCRAPER_API_KEY", "")
 
